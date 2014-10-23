3f1806e345cb1cee5843056cef495dfd211fbf45
Rex Taylor <rex@lightmedia.com.au>
2014-10-23 12:01:29 +0800

    Move to package root

--- examples/views/user/index.blade.php
+++ examples/views/user/index.blade.php
@@ -1,9 +1,9 @@
-@extends('_partials._layout')
+@extends('..._partials._layout')
 
 @section('content')
     <!-- Main Content -->
     <form action="{{ $list->getBaseURL() }}" method="get" class="tblist-form" autocomplete="off" id="users_tblist_form">
-        @include('_partials._tblist')
+        @include('..._partials._tblist')
     </form>
 @stop
 
